0a3bd725d39cac91cdb7faa6257c2c75c861e022
gentledot <gentledot.wp@gmail.com>
2019-12-18 00:42:41 +0900

    Spring Security 학습 - demo project 최초 생성

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Spring Security 학습
+
+## 목표
+1. Spring Security Form 인증 학습
+1. Spring Security의 아키텍쳐 확인
+1. Web Application Security 정리
+1. thymeleaf로 view 생성 방식 정리 (간단정리)
+
+## 참고자료
+- 강의 :
+    - [스프링 시큐리티 / 백기선](https://www.inflearn.com/course/%EB%B0%B1%EA%B8%B0%EC%84%A0-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%8B%9C%ED%81%90%EB%A6%AC%ED%8B%B0/lecture/22894)  
+
---
+++ src/main/java/net/gentledot/demospringsecurity/form/SampleController.java
@@ -0,0 +1,41 @@
+package net.gentledot.demospringsecurity.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal){
+        String message = "Hello, Spring Security.";
+        if (principal != null){
+            message = "Hello, " + principal.getName();
+        }
+
+        model.addAttribute("message", message);
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model){
+        model.addAttribute("message", "Hello, this is info page");
+        return "sample/info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal){
+        model.addAttribute("message", "Hello, " + principal.getName());
+        return "sample/dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal){
+        model.addAttribute("message", "Hello, " + principal.getName());
+        return "sample/admin";
+    }
+
+}
